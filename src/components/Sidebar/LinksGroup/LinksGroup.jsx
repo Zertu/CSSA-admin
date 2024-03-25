dc08cb0d9e8c448564fb4edb5e19e0bf789d30e9
zertu <aa7864548@gmail.com>
2024-03-25 13:57:00 -0230

    Refactor LinksGroup component to use useLocation hook

--- src/components/Sidebar/LinksGroup/LinksGroup.jsx
+++ src/components/Sidebar/LinksGroup/LinksGroup.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import cx from 'classnames';
-import PropTypes from 'prop-types';
-import { NavLink, useMatch } from 'react-router-dom';
-import { Collapse } from 'reactstrap';
+import React, { useState } from "react";
+import cx from "classnames";
+import PropTypes from "prop-types";
+import { NavLink, useLocation } from "react-router-dom";
+import { Collapse } from "reactstrap";
 
-import Icon from '../../Icon/Icon';
+import Icon from "../../Icon/Icon";
 
-import s from './LinksGroup.module.scss';
+import s from "./LinksGroup.module.scss";
 
 function LinksGroup({ header, headerLink, childrenLinks, glyph, className }) {
   const [isOpen, setIsOpen] = useState(false);
-  const match = useMatch(headerLink);
-
+  const location = useLocation();
+  const [match, setMatch] = useState(location?.pathname === headerLink);
+  React.useEffect(() => {
+    setMatch(location?.pathname === headerLink);
+  }, [location, headerLink]);
   if (!childrenLinks) {
     return (
       <li className={cx(s.headerLink, className)}>
-        <NavLink
-          to={headerLink}
-          activeclassname={s.headerLinkActive}
-        >
+        <NavLink to={headerLink} activeclassname={s.headerLinkActive}>
           <div>
             {glyph && <Icon glyph={glyph} />}
             <span>{header}</span>
@@ -31,29 +31,33 @@ function LinksGroup({ header, headerLink, childrenLinks, glyph, className }) {
   return (
     <li className={cx(s.headerLink, className)}>
       <a
-        className={cx({[s.headerLinkActive]: !!match })}
+        className={cx({ [s.headerLinkActive]: !!match })}
         onClick={() => setIsOpen(!isOpen)}
       >
         <div>
           {glyph && <Icon glyph={glyph} />}
           <span>{header}</span>
         </div>
-        <b className={cx('fa fa-angle-left arrow', s.arrow, {[s.arrowActive]: isOpen})} />
+        <b
+          className={cx("fa fa-angle-left arrow", s.arrow, {
+            [s.arrowActive]: isOpen,
+          })}
+        />
       </a>
       <Collapse className={s.panel} isOpen={isOpen}>
         <ul>
           {childrenLinks &&
-          childrenLinks.map(child => (
-            <li key={child.name}>
-              <NavLink
-                to={child.link}
-                onClick={() => setIsOpen(true)}
-                activeclassname={s.headerLinkActive}
-              >
-                {child.name}
-              </NavLink>
-            </li>
-          ))}
+            childrenLinks.map((child) => (
+              <li key={child.name}>
+                <NavLink
+                  to={child.link}
+                  onClick={() => setIsOpen(true)}
+                  activeclassname={s.headerLinkActive}
+                >
+                  {child.name}
+                </NavLink>
+              </li>
+            ))}
         </ul>
       </Collapse>
     </li>
@@ -71,7 +75,7 @@ LinksGroup.propTypes = {
 LinksGroup.defaultProps = {
   headerLink: null,
   childrenLinks: null,
-  className: '',
+  className: "",
   glyph: null,
 };
 
