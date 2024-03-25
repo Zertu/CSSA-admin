2f2975c71bb89cd7a05c1d72c15818967a710e8f
zertu <aa7864548@gmail.com>
2024-03-25 20:16:07 -0230

    Refactor Sidebar component to use dynamic links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,60 +6,71 @@ import Icon from '../Icon/Icon';
 import LinksGroup from './LinksGroup/LinksGroup';
 
 import s from './Sidebar.module.scss';
+const links = [
+  {
+    header: "Dashboard",
+    headerLink: "/app/main",
+    glyph: "dashboard"
+  },
+  {
+    header: "Typography",
+    headerLink: "/app/typography",
+    glyph: "typography"
+  },
+  {
+    header: "Tables Basic",
+    headerLink: "/app/tables",
+    glyph: "tables"
+  },
+  {
+    header: "Notifications",
+    headerLink: "/app/notifications",
+    glyph: "notifications"
+  },
+  {
+    header: "Components",
+    headerLink: "/app/components",
+    glyph: "components",
+    childrenLinks: [
+      {
+        name: 'Buttons',
+        link: '/app/components/buttons',
+      },
+      {
+        name: 'Charts',
+        link: '/app/components/charts',
+      },
+      {
+        name: 'Icons',
+        link: '/app/components/icons',
+      },
+      {
+        name: 'Maps',
+        link: '/app/components/maps',
+      },
+    ]
+  }
+];
 
 const Sidebar = () => (
-  <nav className={s.root}>
-    <header className={s.logo}>
-      <Link to="/app/main">
-        <Icon glyph="logo" />
-      </Link>
-    </header>
-    <ul className={s.nav}>
+<nav className={s.root}>
+  <header className={s.logo}>
+    <Link to="/app/main">
+      <Icon glyph="logo" />
+    </Link>
+  </header>
+  <ul className={s.nav}>
+    {links.map(link => (
       <LinksGroup
-        header="Dashboard"
-        headerLink="/app/main"
-        glyph="dashboard"
+        key={link.header}
+        header={link.header}
+        headerLink={link.headerLink}
+        glyph={link.glyph}
+        childrenLinks={link.childrenLinks}
       />
-      <LinksGroup
-        header="Typography"
-        headerLink="/app/typography"
-        glyph="typography"
-      />
-      <LinksGroup
-        header="Tables Basic"
-        headerLink="/app/tables"
-        glyph="tables"
-      />
-      <LinksGroup
-        header="Notifications"
-        headerLink="/app/notifications"
-        glyph="notifications"
-      />
-      <LinksGroup
-        header="Components"
-        headerLink="/app/components"
-        childrenLinks={[
-          {
-            name: 'Buttons',
-            link: '/app/components/buttons',
-          },
-          {
-            name: 'Charts',
-            link: '/app/components/charts',
-          },
-          {
-            name: 'Icons',
-            link: '/app/components/icons',
-          },
-          {
-            name: 'Maps',
-            link: '/app/components/maps',
-          },
-        ]}
-        glyph="components"
-      />
-    </ul>
-  </nav>
+    ))}
+  </ul>
+</nav>
 );
 
 function mapStateToProps(store) {
